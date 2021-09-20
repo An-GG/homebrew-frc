1a6538a42191f2251ff04a3e786506344eee8f24
Ankush Girotra <ankush.girotra@gmail.com>
2021-09-20 04:22:59 -0500

    Update arm-frc-linux-gnueabi-gcc.rb

--- arm-frc-linux-gnueabi-gcc.rb
+++ arm-frc-linux-gnueabi-gcc.rb
@@ -48,7 +48,7 @@ class ArmFrcLinuxGnueabiGcc < Formula
 
     # Symlink with binutils so that GCC can find
     # the right assembler and linker
-    ln_s "#{Formula["binutils-frc"].opt_prefix}/#{target}", "#{prefix}/#{target}"
+    ln_s "#{Formula["arm-linux-gnueabihf-binutils"].opt_prefix}/#{target}", "#{prefix}/#{target}"
 
     mkdir 'build' do
       system "../configure", *args
